e0bae68506b4f240f792fc035527cb2e50141822
Richard Mott <ravensgrim@gmail.com>
2022-03-27 14:49:03 -0500

    Delete old data classes

--- GausBestiary.App/Models/RageSkills/HealingSpell.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GausBestiary.App.Models.RageSkills
-{
-    public class HealingSpell : ISkill
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Power { get; set; }
-    }
-}
--- GausBestiary.App/Models/RageSkills/MeleeAttack.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GausBestiary.App.Models.RageSkills
-{
-    public class MeleeAttack : ISkill
-    {
-        public string Name { get; set; }
-        public int Level { get; set; }
-        public string Power { get; set; }
-    }
-}
